04e220743a587f2e529c445cba36ec251994618b
Adrià Belmonte Navarro <adriabn@TTC.CAT>
2019-12-19 12:09:40 +0100

    Second commit actually working POG

---
+++ TestGitHub1/Source1.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//git clone url para copiar el directorio de github al pc
+
+int main() {
+	printf("Hello world of failing kek \n");
+
+	system("pause");
+	return 0;
+
+}
+
+
+//git status para info
+//git commit cuando has hecho "git add" para subir al repositorio local con -m "" para añadir un mensaje 
+//git push para subir al servidor el programa
